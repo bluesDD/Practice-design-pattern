0cde5486e4dd61af22c5880766909a615dd6253a
bluesDD <dd11blues@gmail.com>
2020-07-11 15:24:31 +0900

    :+1: testcode追加

--- edit_spreadsheet/tests/test_edit_spreadsheet.py
+++ edit_spreadsheet/tests/test_edit_spreadsheet.py
@@ -1,5 +1,11 @@
-from edit_spreadsheet import __version__
-
+from edit_spreadsheet import __version__, edit_spreadsheet
 
 def test_version():
     assert __version__ == '0.1.0'
+
+user_list = [["XXXX", "YYYY", "ZZZZ"], ["1111", "2YYYY", "3ZZZZ"]]
+
+parser = edit_spreadsheet.SudoUserParser(user_list)
+
+def test_append_sudo_users_works_fine():
+  assert parser.append_sudo_users() == [['XXXX', 'YYYY', 'ZZZZ', 'Sudo権限'], ['1111', '2YYYY', '3ZZZZ', False]]
