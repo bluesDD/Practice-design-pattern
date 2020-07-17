20aa148c2f3e5d13d6e271305b6c5331d4a76cd6
bluesDD <dd11blues@gmail.com>
2020-07-18 01:36:46 +0900

    :tada: factory method

---
+++ factory_method/Main.py
@@ -0,0 +1,49 @@
+from abc import abstractmethod, ABCMeta
+
+class Dialog:
+  @abstractmethod
+  def createButton(self):
+    pass
+  
+  def render(self):
+    okButton = self.createButton()
+    okButton.onClick(WindowDialog())
+    okButton.render()
+
+
+class WindowDialog(Dialog):
+  def createButton(self):
+    return WindowsButton()
+
+
+class Button(metaclass=ABCMeta):
+  @abstractmethod
+  def render(self):
+    pass
+  
+  @abstractmethod
+  def onClick(self):
+    pass
+
+
+class WindowsButton(Button):
+  def render(self):
+    return print("Window button rended")
+
+  def onClick(self, f):
+    return print("Window On Click")
+
+
+class Application:
+
+  def __init__(self, os):
+    if os == "Windows":
+      self.dialog = WindowDialog()
+
+  def main(self):
+    self.dialog.render()
+
+if __name__ == "__main__":
+  print("App started")
+  app = Application("Windows")
+  app.main()
