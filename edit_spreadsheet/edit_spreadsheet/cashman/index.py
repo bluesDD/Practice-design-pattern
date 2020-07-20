2cfc47002ac9c6ce5647eab28b694272330a32ab
bluesDD <dd11blues@gmail.com>
2020-07-21 00:02:52 +0900

    :tada: add user mode;

--- edit_spreadsheet/edit_spreadsheet/cashman/index.py
+++ edit_spreadsheet/edit_spreadsheet/cashman/index.py
@@ -2,7 +2,9 @@ from flask import Flask, jsonify, request
 from .model.expense import Expense, ExpenseSchema
 from .model.income import Income, IncomeSchema
 from .model.transaction_type import TransactionType
+from .model.user import User, UserSchema
 import sys
+
 
 app = Flask(__name__)
 
@@ -13,7 +15,10 @@ transactions = [
   Expense("Pasta", 50),
   Expense("Rock Concert", 100)
 ]
-
+user_data = {
+    "email": "ken@yahoo.com",
+    "name": "Ken",
+}
 @app.route('/')
 def index():
   return "Hi guys"
@@ -46,6 +51,11 @@ def add_expense():
   transactions.append(expense)
   return "", 204
 
+@app.route('/user')
+def get_user():
+  schema = UserSchema()
+  user = schema.dump(user_data)
+  return jsonify(user)
 
 
 if __name__ == "__main__":
