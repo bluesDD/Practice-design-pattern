c448a77bd54ab00c527596bfe9111b467018c4b5
bluesDD <dd11blues@gmail.com>
2020-07-24 02:25:32 +0900

    ✅ Indexのテスト追加

--- edit_spreadsheet/tests/test_edit_spreadsheet.py
+++ edit_spreadsheet/tests/test_edit_spreadsheet.py
@@ -1,4 +1,6 @@
 from edit_spreadsheet import __version__, edit_spreadsheet
+from edit_spreadsheet.cashman.index import get_newuser, app
+
 
 def test_version():
     assert __version__ == '0.1.0'
@@ -9,3 +11,9 @@ parser = edit_spreadsheet.SudoUserParser(user_list)
 
 def test_append_sudo_users_works_fine():
   assert parser.append_sudo_users() == [['XXXX', 'YYYY', 'ZZZZ', 'Sudo権限'], ['1111', '2YYYY', '3ZZZZ', False]]
+
+def test_flask_api():
+  response = app.test_client.get("/")
+  assert response.status_code == 200
+  assert response.data == b'Hi guys'
+
