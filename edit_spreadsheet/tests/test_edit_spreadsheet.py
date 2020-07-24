e94459c75d7de89f0fd1977ff0b9f1ec8b44bc09
bluesDD <dd11blues@gmail.com>
2020-07-25 00:01:54 +0900

    ✅ fixtureの追加

--- edit_spreadsheet/tests/test_edit_spreadsheet.py
+++ edit_spreadsheet/tests/test_edit_spreadsheet.py
@@ -1,5 +1,7 @@
 from edit_spreadsheet import __version__, edit_spreadsheet
 from edit_spreadsheet.cashman.index import get_newuser, app
+from edit_spreadsheet.cashman.model.user import NewUser, NewUserSchema
+import pytest
 
 
 def test_version():
@@ -12,8 +14,17 @@ parser = edit_spreadsheet.SudoUserParser(user_list)
 def test_append_sudo_users_works_fine():
   assert parser.append_sudo_users() == [['XXXX', 'YYYY', 'ZZZZ', 'Sudo権限'], ['1111', '2YYYY', '3ZZZZ', False]]
 
+@pytest.fixture
+def test_db():
+  
+
 def test_flask_api_routing():
   with app.test_client() as c:
     response = c.get("/")
     assert response.status_code == 200
     assert response.data == b'Hi guys'
+
+  with app.test_client() as c:
+    newuser_response = c.get("/newuser")
+    assert newuser_response.status_code == 200
+
