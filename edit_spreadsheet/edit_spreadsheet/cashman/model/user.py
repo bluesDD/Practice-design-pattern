aeec7079db465d149090eab4aad3853a5c955fd5
bluesDD <dd11blues@gmail.com>
2020-07-27 23:22:20 +0900

    :+1: add user

--- edit_spreadsheet/edit_spreadsheet/cashman/model/user.py
+++ edit_spreadsheet/edit_spreadsheet/cashman/model/user.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask.sqlite"
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
 # 参考：https://marshmallow.readthedocs.io/en/stable/quickstart.html
@@ -15,10 +15,15 @@ def init_db(app):
   db.create_engine("sqlite:///sample_db.sqlite3")
   db.init_app(app)
   db.create_all()
+
 class NewUser(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   first = db.Column(db.String(80))
   last = db.Column(db.String(80))
+
+  def add_user(self, first, last):
+    self.first = first
+    self.last = las
 
 class NewUserSchema(Schema):
   id = fields.Int(dump_only=True)
@@ -28,6 +33,10 @@ class NewUserSchema(Schema):
 
   def format_name(self, newuser):
     return "{}, {}".format(newuser.last, newuser)
+
+  @post_load
+  def make_newuser(self, data, **kwargs):
+    return NewUser(**data)
 
 def must_not_be_blank(data):
   if not data:
