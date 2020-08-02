4b4aa629dd92506615e5d361de06493be1e7a4c2
bluesDD <dd11blues@gmail.com>
2020-08-02 23:54:05 +0900

    :tada: strategyパターンの完成。

--- strategy/Main.py
+++ strategy/Main.py
@@ -1,7 +1,7 @@
 from abc import ABCMeta, abstractmethod
 
 
-class Strategy(ABCMeta):
+class Strategy(metaclass=ABCMeta):
   @abstractmethod
   def execute(self, a: int, b: int) -> int:
     pass
@@ -27,3 +27,29 @@ class Context:
 
   def executeStrategy(self, a: int, b: int):
     return self.__strategy.execute(a, b)
+
+
+def main():
+  user_input = input("Enter your application name(add/sub/multiply)")
+  user_input_int_1 = input("Enter your first number: ")
+  user_input_int_2 = input("Enter your second number: ")
+
+  context = Context()
+
+  if user_input == "add" :
+    add = ConcreteStarategyAdd()
+    context.setStrategy(add)
+  elif user_input == "sub":
+    sub = ConcreteStarategySubstract()
+    context.setStrategy(sub)
+  else:
+    multi = ConcreteStarategyMultiply()
+    context.setStrategy(multi)
+
+  result = context.executeStrategy(int(user_input_int_1), int(user_input_int_2))
+
+  print(result)
+
+
+if __name__ == "__main__":
+  main()
