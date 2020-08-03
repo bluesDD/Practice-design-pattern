911528ff874632f958802d576a0a38b685f059df
bluesDD <dd11blues@gmail.com>
2020-08-03 23:42:17 +0900

    :+1: factory_method_v2

---
+++ factory_method/MainSecond.py
@@ -0,0 +1,56 @@
+from abc import abstractmethod, ABCMeta
+
+
+
+class Dialog:
+  @abstractmethod
+  def createButton(self):
+    pass
+
+  def render(self):
+    okButton = self.createButton()
+    okButton.onClick(WindowDialog())
+    okButton.render()
+
+
+
+class WindowDialog(Dialog):
+  def createButton(self):
+    return WindowsButton()
+
+
+
+class Button(metaClass=ABCMeta):
+  @abstractmethod
+  def render(self):
+    pass
+
+  @abstractmethod
+  def onClick(self):
+    pass
+
+
+
+class WindowsButton(Button):
+  def render(self):
+    return print("Window Button renderd")
+
+  def onClick(self):
+    return print("Window On Click")
+
+
+
+class Application:
+
+  def __init__(self, os):
+    if os == "Windows":
+      self.dialog = WindowDialog()
+
+  def main(self):
+    self.dialog.render()
+
+
+if __name__ == "__main__":
+  print("App started")
+  app = (Application("Windows"))
+  app.main()
