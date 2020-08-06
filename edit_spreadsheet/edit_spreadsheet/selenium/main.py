8602343f495cb4a84a947abc2683987636ab9875
bluesDD <dd11blues@gmail.com>
2020-08-07 01:10:24 +0900

    :wrench: chromedriverのバージョン指定

--- edit_spreadsheet/edit_spreadsheet/selenium/main.py
+++ edit_spreadsheet/edit_spreadsheet/selenium/main.py
@@ -1,13 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
+import chromedriver_binary
+import time
 
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-driver.close()
+driver = webdriver.Chrome() 
+driver.get('https://qiita.com/')
+time.sleep(5)
+driver.quit()
