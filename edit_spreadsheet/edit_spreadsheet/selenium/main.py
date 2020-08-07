a856f946e37c6f76429c7f0ee3dd208e3f0aac48
bluesDD <dd11blues@gmail.com>
2020-08-07 23:55:22 +0900

    :+1: ログイン処理

--- edit_spreadsheet/edit_spreadsheet/selenium/main.py
+++ edit_spreadsheet/edit_spreadsheet/selenium/main.py
@@ -4,6 +4,8 @@ import time
 
 
 driver = webdriver.Chrome() 
-driver.get('https://qiita.com/')
+driver.get('https://members.classmethod.net/#!/login')
+id = driver.find_element_by_id("loginID")
+
 time.sleep(5)
 driver.quit()
