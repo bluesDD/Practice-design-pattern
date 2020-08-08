f2f12b974380b99a8bb84644de7db37b4c8f3df0
bluesDD <dd11blues@gmail.com>
2020-08-08 23:17:20 +0900

    :+1: login pw

--- edit_spreadsheet/edit_spreadsheet/selenium/main.py
+++ edit_spreadsheet/edit_spreadsheet/selenium/main.py
@@ -6,6 +6,6 @@ import time
 driver = webdriver.Chrome() 
 driver.get('https://members.classmethod.net/#!/login')
 id = driver.find_element_by_id("loginID")
-
+password = driver.find_element_by_id("loginPW")
 time.sleep(5)
 driver.quit()
