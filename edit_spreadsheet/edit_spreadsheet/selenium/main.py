e11604ceab93744a022e96de6a1c03f1c16e3d2e
bluesDD <dd11blues@gmail.com>
2020-08-10 02:35:18 +0900

    :bug: タイムアウト処理を入れて、useridが取得できるようになった１

--- edit_spreadsheet/edit_spreadsheet/selenium/main.py
+++ edit_spreadsheet/edit_spreadsheet/selenium/main.py
@@ -1,13 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 import chromedriver_binary
 import time
 
 
 driver = webdriver.Chrome() 
 driver.get('https://members.classmethod.net/#!/login')
-print(driver.current_url)
+
+try:
+  id = WebDriverWait(driver, 15).until(
+    expected_conditions.presence_of_element_located((By.ID, "loginID"))
+  )
+  print(id)
+  print(driver.title)
+except TimeoutError:
+  print("Timeout")
+
 wrapper = driver.find_element_by_id("wrapper")
-# id = driver.find_element_by_id("loginID")
+id = driver.find_element_by_id("loginID")
+print(id)
 # password = driver.find_element_by_id("loginPW")
 time.sleep(5)
 driver.quit()
