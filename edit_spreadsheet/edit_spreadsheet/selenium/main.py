d4246857b7c21f6a2396b274c58cd33ba3dc9362
bluesDD <dd11blues@gmail.com>
2020-08-10 23:48:17 +0900

    :+1: いらない処理抜いた

--- edit_spreadsheet/edit_spreadsheet/selenium/main.py
+++ edit_spreadsheet/edit_spreadsheet/selenium/main.py
@@ -17,12 +17,10 @@ try:
   password = WebDriverWait(driver, 15).until(
     expected_conditions.presence_of_element_located((By.ID, "loginPW"))
   )
+  print(password)
 except TimeoutError:
   print("Timeout")
 
-wrapper = driver.find_element_by_id("wrapper")
-id = driver.find_element_by_id("loginID")
-print(id)
 # password = driver.find_element_by_id("loginPW")
 time.sleep(5)
 driver.quit()
