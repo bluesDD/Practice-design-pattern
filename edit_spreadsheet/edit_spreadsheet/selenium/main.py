43972178fcead64f7da7e8a6cd2c3619cedfa11d
bluesDD <dd11blues@gmail.com>
2020-08-11 23:58:35 +0900

    :+1: btnクリック処理

--- edit_spreadsheet/edit_spreadsheet/selenium/main.py
+++ edit_spreadsheet/edit_spreadsheet/selenium/main.py
@@ -4,23 +4,30 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import chromedriver_binary
 import time
+import sys
 
-
+target_url = "https://members.classmethod.net/#!/login"
+user_id = "test"
+user_password = "test"
+element_id_user_id = "loginID"
+element_id_user_password = "loginPW"
+element_class_login_button = "btn"
 driver = webdriver.Chrome() 
-driver.get('https://members.classmethod.net/#!/login')
+driver.get(target_url)
 
 try:
   id = WebDriverWait(driver, 15).until(
-    expected_conditions.presence_of_element_located((By.ID, "loginID"))
+    expected_conditions.presence_of_element_located((By.ID, element_id_user_id))
   )
-  print(id)
+  id.send_keys(user_id)
   password = WebDriverWait(driver, 15).until(
-    expected_conditions.presence_of_element_located((By.ID, "loginPW"))
+    expected_conditions.presence_of_element_located((By.ID, element_id_user_password))
   )
-  print(password)
-except TimeoutError:
-  print("Timeout")
+  password.send_keys(user_password)
+  driver.find_element_by_class_name(element_class_login_button).click()
 
-# password = driver.find_element_by_id("loginPW")
+except TimeoutError:
+  sys.exit("Timeout")
+
 time.sleep(5)
 driver.quit()
