d231bf8cf48736fedb7882621a1f1e4dffe4edac
bluesDD <dd11blues@gmail.com>
2020-09-24 23:52:02 +0900

    :+1: 日時計算

--- utils/change_timezone.py
+++ utils/change_timezone.py
@@ -1,10 +1,13 @@
-from datetime import timedelta, date
+from datetime import timedelta, date, datetime
 
 now = date.today()
 print(now)
-day = date.today() + timedelta(hours=-24)
+day = date.today() + timedelta(hours=-23)
 some = day.strftime("%Y-%m-%d")
 print(some)
-day = date.today() + timedelta(hours=+24)
+day = date.today() + timedelta(hours=+24*8)
+some = day.strftime("%Y-%m-%d")
+print(some)
+day = datetime.now() + timedelta(hours=+2)
 some = day.strftime("%Y-%m-%d")
 print(some)
