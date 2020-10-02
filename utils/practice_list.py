0b8f12f9c85f0a67d8fea842db36c6c8db2e6aeb
bluesDD <dd11blues@gmail.com>
2020-10-03 00:10:36 +0900

    tmp

--- utils/practice_list.py
+++ utils/practice_list.py
@@ -3,3 +3,9 @@
 target = [4, 18, 25, 20, "9", "40"]
 target = list(map(int, target))
 print(sorted(target, reverse=True)[3])
+
+sub = [6,1,5,6,3,2,6]
+a = [print(1) for _ in range(len(sub))]
+print(a)
+flag = False
+candidate = []
