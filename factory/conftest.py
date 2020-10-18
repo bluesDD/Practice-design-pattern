6fc934fdbc9613a9a499b8e04f680c785952b4d7
bluesDD <dd11blues@gmail.com>
2020-10-18 23:09:02 +0900

    :+1: addd addoption

--- factory/conftest.py
+++ factory/conftest.py
@@ -3,3 +3,11 @@ from sqlalchemy import orm
 
 import pytest
 from models import Base, User, Address
+
+
+SESSION = orm.scoped_session(orm.sessionmaker())
+BOUND = False
+
+
+def pytest_addoption(paraser):
+    paraser.addoption("--small_only", action="store_true")
