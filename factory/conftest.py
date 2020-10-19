30515f06e466d8a23685cc805772c3f02ed60d04
bluesDD <dd11blues@gmail.com>
2020-10-19 23:20:41 +0900

    :+1: add sqlalchemy

--- factory/conftest.py
+++ factory/conftest.py
@@ -11,3 +11,14 @@ BOUND = False
 
 def pytest_addoption(paraser):
     paraser.addoption("--small_only", action="store_true")
+
+def pytest_configure(config):
+    config.addinivalue_line("makers", "small: Small test")
+
+def pytest_runtest_setup(item):
+    small_only = item.config.getoption("small_only")
+    if not small_only:
+        global SESSION, BOUND
+        if not BOUND:
+            engine = sqlalchemy.create_engine("sqlite://")
+
