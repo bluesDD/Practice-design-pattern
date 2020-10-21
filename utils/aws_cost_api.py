8618ebf1c755715079182143f63c3c6f10334afd
bluesDD <dd11blues@gmail.com>
2020-10-21 23:01:05 +0900

    :tada: price list

--- utils/aws_cost_api.py
+++ utils/aws_cost_api.py
@@ -5,6 +5,20 @@ import pprint
 pricing = boto3.client("pricing", region_name="us-east-1")
 
 res = pricing.describe_services()
-for service in res["Services"]:
-  print(service["ServiceCode"])
+# for service in res["Services"]:
+#   print(service["ServiceCode"])
 
+res = pricing.get_products(
+  ServiceCode="AmazonEC2",
+  Filters=[
+    {
+      'Type': 'TERM_MATCH',
+      'Field': 'operatingSystem',
+      'Value': 'Linux'
+    }
+  ])
+
+price_items = json.loads(res["PriceList"][0])
+terms = price_items["terms"]
+print(price_items)
+print(terms)
