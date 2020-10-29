74c45f46f7253be112aa0563dfd230fcf52a0539
bluesDD <dd11blues@gmail.com>
2020-10-29 23:27:11 +0900

    :+1: testing sendgrid

--- utils/sendgrid_api_key.py
+++ utils/sendgrid_api_key.py
@@ -3,3 +3,8 @@ import os
 
 
 sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+res = sg.client.user.account.get()
+print(res.status_code)
+#200
+print(res.body)
+#b'{"is_reseller_customer":true,"reputation":100,"type":"free"}'
