4247806ed9d498266d149e2e1a9ca6248cfd42b3
bluesDD <dd11blues@gmail.com>
2020-10-30 23:38:14 +0900

    :+1: API keyの発行

--- utils/sendgrid_api_key.py
+++ utils/sendgrid_api_key.py
@@ -1,10 +1,19 @@
 from sendgrid import SendGridAPIClient
 import os
-
+import requests
 
 sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-res = sg.client.user.account.get()
+
+data = {
+  "name": "Test key",
+  "sample": "data",
+  "scopes": [
+    "teammates.read"
+  ]
+}
+
+res = sg.client.api_keys.post(request_body=data)
 print(res.status_code)
 #200
 print(res.body)
-#b'{"is_reseller_customer":true,"reputation":100,"type":"free"}'
+b'{"is_reseller_customer":true,"reputation":100,"type":"free"}'
