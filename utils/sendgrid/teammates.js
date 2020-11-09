b27b0ec819d611e5ec94e2dfbd2231773b42d84b
bluesDD <dd11blues@gmail.com>
2020-11-09 23:41:08 +0900

    :tada: gasでsendgrid teammatesを叩く

---
+++ utils/sendgrid/teammates.js
@@ -0,0 +1,34 @@
+SENDGRID_TEAMMATES_READONLY_API_KEY = ""
+SENDGRID_BASE_ENDPOINT = "https://api.sendgrid.com/v3"
+SENDGRID_TEAMMATES_PATH = "/teammates"
+SENDGRID_TEAMMATES_ENDPOINT = SENDGRID_BASE_ENDPOINT + SENDGRID_TEAMMATES_PATH
+
+function debug() {
+  Logger.log(SENDGRID_TEAMMATES_ENDPOINT);
+  Logger.log("This");
+}
+
+
+
+function retrieve_teammates() {
+  Logger.log("Starting Retrieve Teammates...");
+  try {
+    const res =   UrlFetchApp.fetch(SENDGRID_TEAMMATES_ENDPOINT, {
+      method: 'GET',
+      headers: { "Content-Type": 'application/json',
+                "Authorization": "Bearer "+ SENDGRID_TEAMMATES_READONLY_API_KEY}
+    });
+    
+    const res_json = res.getContentText();
+    const res_text = JSON.parse(res_json);
+    
+    Logger.log("Retrieve teammates completed!");
+    for (var i in res_text.result) {
+      Logger.log(res_text.result[i].email);
+    }
+  } catch(e) {
+    Logger.log("Retrieving teammates failed...")
+    Logger.log(e)
+  }
+}
+
