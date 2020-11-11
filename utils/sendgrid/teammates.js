bdb5a10aa55a6b0f92a8142865694dded6c05252
bluesDD <dd11blues@gmail.com>
2020-11-11 23:12:18 +0900

    :recycle: 余分な関数削除

--- utils/sendgrid/teammates.js
+++ utils/sendgrid/teammates.js
@@ -3,15 +3,10 @@ SENDGRID_BASE_ENDPOINT = "https://api.sendgrid.com/v3"
 SENDGRID_TEAMMATES_PATH = "/teammates"
 SENDGRID_TEAMMATES_ENDPOINT = SENDGRID_BASE_ENDPOINT + SENDGRID_TEAMMATES_PATH
 
-function debug() {
-  Logger.log(SENDGRID_TEAMMATES_ENDPOINT);
-  Logger.log("This");
-}
-
-
 
 function retrieve_teammates() {
   Logger.log("Starting Retrieve Teammates...");
+
   try {
     const res =   UrlFetchApp.fetch(SENDGRID_TEAMMATES_ENDPOINT, {
       method: 'GET',
@@ -22,6 +17,7 @@ function retrieve_teammates() {
     const res_text = JSON.parse(res.getContentText());
     
     Logger.log("Retrieve teammates completed!");
+
     for (var i in res_text.result) {
       Logger.log(res_text.result[i].email);
     }
