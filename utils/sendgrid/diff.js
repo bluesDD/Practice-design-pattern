fee8f21b14cc5fb34c5f77d0e06b37ac75461cbb
bluesDD <dd11blues@gmail.com>
2020-11-29 23:55:56 +0900

    :+1: 日付比較

--- utils/sendgrid/diff.js
+++ utils/sendgrid/diff.js
@@ -21,3 +21,9 @@ function get_today () {
   var today = Utilities.formatDate(date, 'Asia/Tokyo', 'yyyyMMdd');
   return today;
 }
+
+function diff_date() {
+  var dt = Math.abs(d1.getTime() - d2.getTime()); // ミリ秒数値を引き算
+  var d = dt / (1000 * 60 * 60 * 24); // １日のミリ秒数で割り算
+  Logger.log(d + "日 "); // ログに出力
+  }
