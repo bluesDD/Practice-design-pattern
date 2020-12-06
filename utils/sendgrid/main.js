299c83c19d5c0c7515faf1d61c7702a47d50cdcd
bluesDD <dd11blues@gmail.com>
2020-12-06 23:43:58 +0900

    :recycle: 変数名をわかりやすく

--- utils/sendgrid/main.js
+++ utils/sendgrid/main.js
@@ -1,13 +1,13 @@
 function main() {
-  var row_num = 4
+  var expiry_date_row = 4
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sh = ss.getActiveSheet();
-  var rows = sh.getRange(1, row_num, sh.getLastRow()).getValues();
-  var dates = Array.prototype.concat.apply([], rows);
+  var row_items = sh.getRange(1, expiry_date_row, sh.getLastRow()).getValues();
+  var expiry_dates = Array.prototype.concat.apply([], row_items);
   var expires_in_30 = false
   var expires_in_40 = false
 
-  dates.map(function(value) {
+  expiry_dates.map(function(value) {
     var now = Moment.moment();
     var expired_day = Moment.moment(value);
     var expires_in = now.diff(expired_day, "d");
