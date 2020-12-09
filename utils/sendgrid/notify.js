d67a77239bde3b1a4d4010b80344cea45a792d3c
bluesDD <dd11blues@gmail.com>
2020-12-09 23:06:22 +0900

    :shower :memo削除

--- utils/sendgrid/notify.js
+++ utils/sendgrid/notify.js
@@ -1,6 +1,6 @@
 function notify_slack(message) {
   var post_url  = "";
-  var slack_message = message + "日以内に期限が切れる証明書・ドメインがあります！→ https://docs.google.com/spreadsheets/d/1aN96iDo_9we9t_LQZ4oxrUna-jQIXvIeie59Ll5fZps/edit#gid=827799759"
+  var slack_message = message + "日以内に期限が切れる証明書・ドメインがあります！→  "
 
   var options = {
     "method" : "POST",
