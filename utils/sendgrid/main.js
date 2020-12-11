3ea48e9e1bdabd45184a7824bfcc95552785b2fc
bluesDD <dd11blues@gmail.com>
2020-12-11 22:58:15 +0900

    :+1: 日数計算を共通化

--- utils/sendgrid/main.js
+++ utils/sendgrid/main.js
@@ -4,27 +4,22 @@ function main() {
   var sh = ss.getActiveSheet();
   var row_items = sh.getRange(1, expiry_date_row, sh.getLastRow()).getValues();
   var expiry_dates = Array.prototype.concat.apply([], row_items);
-  var expires_in_30 = false
-  var expires_in_40 = false
+  var remaining_days = 0;
 
   expiry_dates.map(function(value) {
     var now = Moment.moment();
     var expired_day = Moment.moment(value);
     var expires_in = now.diff(expired_day, "d");
     if (expires_in < 30) {
-      expires_in_30 = true
+      remaining_days = 30
     }
     if (expires_in < 40) {
-      expires_in_40 = true
+      remaining_days = 40
     }
   })
   
-  if (expires_in_30) {
-    notify_slack(30)
-    expires_in_40 = false
-  }
-  if (expires_in_40) {
-    notify_slack(40)
+  if (remaining_days) {
+    notify_slack(remaining_days)
   }
   
 }
