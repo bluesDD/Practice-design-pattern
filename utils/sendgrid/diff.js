e9ded8d57ec15aebb8fe16569492bdf674ec89bd
bluesDD <dd11blues@gmail.com>
2020-12-12 23:57:51 +0900

    :shower: 一行消した

--- utils/sendgrid/diff.js
+++ utils/sendgrid/diff.js
@@ -2,9 +2,8 @@ function get_diff() {
   const array1 = [1, 2, 3, 4, 5, 6];
   const array2 = [1, 2, 3, 4];
 
-
   array3 = array1.filter(i => array2.indexOf(i) == -1)
-  return array3; // [5, 6]
+  return array3;
 }
 
 
