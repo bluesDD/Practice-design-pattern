0a2f7b6aa3799c0956cdafce6833452245883780
bluesDD <dd11blues@gmail.com>
2020-12-21 23:36:21 +0900

    :+1: teammates叩く

---
+++ utils/sendgrid_teammates.py
@@ -0,0 +1,29 @@
+from sendgrid import SendGridAPIClient
+import os
+import requests
+import json
+
+def set_api_key_env():
+  base_url = "https://api.sendgrid.com/v3"
+  sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+
+  data = {
+    "name": "Test key",
+    "sample": "data",
+    "scopes": [
+      "teammates.read"
+    ]
+  }
+
+  res = sg.client.api_keys.post(request_body=data)
+  os.environ['SENDGRID_TEAMMATES_API_KEY'] = json.loads(res.body)["api_key"]
+  return print('API Key set in env.')
+
+def get_teammates():
+  api_key = os.environ.get('SENDGRID_TEAMMATES_API_KEY')
+  sg = SendGridAPIClient(api_key)
+  res = sg.client.teammates.get()
+  return json.loads(res.body)
+
+if __name__ == "__main__":
+  print(get_teammates())
