a67c98728478c0e3949039dc3d205108824a2127
bluesDD <dd11blues@gmail.com>
2021-01-07 00:01:33 +0900

    :+1: get key from github

--- utils/github_public_key.py
+++ utils/github_public_key.py
@@ -3,7 +3,14 @@ import json
 
 
 BASE_URL = "https://github.com/"
-username = "bluesDD"
-r = requests.get(BASE_URL + username + ".keys")
-print(r.text)
+usernames = ["bluesDD", "bluesDD"]
+
+def get_public_keys_from_github(usernames):
+  for username in usernames:
+    r = requests.get(BASE_URL + username + ".keys")
+    print(r.text)
 # public key
+
+
+if __name__ == "__main__":
+  get_public_keys_from_github(usernames)
