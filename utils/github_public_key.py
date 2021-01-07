1a7558010b53764bafc5696a691684dab9a09580
bluesDD <dd11blues@gmail.com>
2021-01-07 23:59:03 +0900

    :+1: array(dict)で返すようにしてみた

--- utils/github_public_key.py
+++ utils/github_public_key.py
@@ -6,11 +6,16 @@ BASE_URL = "https://github.com/"
 usernames = ["bluesDD", "bluesDD"]
 
 def get_public_keys_from_github(usernames):
+  keys = []
   for username in usernames:
     r = requests.get(BASE_URL + username + ".keys")
-    print(r.text)
-# public key
+    keys.append({
+      "user": username,
+      "key": r.text
+    })
+  return keys
 
 
 if __name__ == "__main__":
-  get_public_keys_from_github(usernames)
+  ke = get_public_keys_from_github(usernames)
+  print(ke)
