52886940bda9aab27fc909938d3569b7d643080c
bluesDD <dd11blues@gmail.com>
2021-01-08 23:51:41 +0900

    :+1: added

--- utils/github_public_key.py
+++ utils/github_public_key.py
@@ -2,13 +2,13 @@ import requests
 import json
 
 
-BASE_URL = "https://github.com/"
+BASE_URL = "https://github.com/bluesDD.keys"
 usernames = ["bluesDD", "bluesDD"]
 
 def get_public_keys_from_github(usernames):
   keys = []
   for username in usernames:
-    r = requests.get(BASE_URL + username + ".keys")
+    r = requests.get(BASE_URL)
     keys.append({
       "user": username,
       "key": r.text
