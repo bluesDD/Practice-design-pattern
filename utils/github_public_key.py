4ae8a909c3ac795bd462089fc3a0f7f23de0c50b
bluesDD <dd11blues@gmail.com>
2021-01-09 23:35:56 +0900

    :+1: バグってしまった

--- utils/github_public_key.py
+++ utils/github_public_key.py
@@ -1,21 +1,36 @@
 import requests
 import json
+import yaml
 
 
 BASE_URL = "https://github.com/bluesDD.keys"
 usernames = ["bluesDD", "bluesDD"]
+YAML_FILE = "test.yml"
 
-def get_public_keys_from_github(usernames):
+def get_public_keys_from_github(users):
   keys = []
-  for username in usernames:
-    r = requests.get(BASE_URL)
+  for user in users:
+    print(user)
+    # if user["state"]:
+    #   if user["state"] == "absent":
+    #     continue
+    res = requests.get(user["authorized_keys"][0]["key"])
     keys.append({
-      "user": username,
-      "key": r.text
+      "user": user["name"],
+      "key": res.text
     })
   return keys
 
 
+def load_yaml(file):
+  with open(file) as f:
+    return yaml.safe_load(f)
+
+
 if __name__ == "__main__":
-  ke = get_public_keys_from_github(usernames)
-  print(ke)
+  top_var = "users"
+  obj = load_yaml(YAML_FILE)
+  print(obj)
+  a = get_public_keys_from_github(obj)
+  print(a)
+  
