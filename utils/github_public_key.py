c86f00437b92b6ef7bc24d4a3d286554d72eff31
bluesDD <dd11blues@gmail.com>
2021-01-12 22:41:09 +0900

    :recycle: リファクタ

--- utils/github_public_key.py
+++ utils/github_public_key.py
@@ -2,17 +2,14 @@ import requests
 import json
 import yaml
 
-
-BASE_URL = "https://github.com/bluesDD.keys"
-usernames = ["bluesDD", "bluesDD"]
 YAML_FILE = "test.yml"
+WEB_HOOK_URL = "https://hooks.slack.com/zzz"
 
 def get_public_keys_from_github(users):
   keys = []
   for user in users:
     if is_state_absent(user):
         continue
-
     res = requests.get(user["authorized_keys"][0]["key"])
     keys.append({
       "user": user["name"],
@@ -39,21 +36,25 @@ def public_key_exists(key):
     return True
 
 def send_message_to_slack(key):
-  message_warning = key["user"] + "さんのGitHub上の公開鍵が消えてしまっているようです。再登録作業を案内してあげてください。"
-  message_confirm_url = "→当該URL： " + key["key_url"]
-  WEB_HOOK_URL = "https://hooks.slack.com/zzz"
+  warning_message = key["user"] \
+    + "さんのGitHub上の公開鍵が消えてしまっているようです。再登録作業を案内してあげてください。\n" \
+    + "→当該URL： " + key["key_url"]
   
-  requests.post(WEB_HOOK_URL, data=json.dumps({
-    "text" : message_warning + "\n" + message_confirm_url,
-  }))
+  try:
+    # TODO: テスト用にかえてる
+    # requests.post(WEB_HOOK_URL, data=json.dumps({
+    #   "text" : warning_message,
+    # }))
+    return print("Warning have been sent to slack successfully!")
+  except:
+    raise
+
 
 
 if __name__ == "__main__":
   obj = load_yaml(YAML_FILE)
   users = obj["users"]
-  print(users)
   keys = get_public_keys_from_github(users)
-  print(keys)
   for key in keys:
     if public_key_exists(key) == False:
       send_message_to_slack(key)
