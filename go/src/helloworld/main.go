a14d524626186baed14da6e280554b254f3f3a84
bluesDD <dd11blues@gmail.com>
2021-04-06 02:24:14 +0900

    :+1: sum

--- go/src/helloworld/main.go
+++ go/src/helloworld/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	sum := sum(os.Args[1], os.Args[2])
+	println("SUm: ", sum)
+}
+
+func sum(number1 string, number2 string) int {
+	int1, _ := strconv.Atoi(number1)
+	int2, _ := strconv.Atoi(number2)
+	return int1 + int2
 }
