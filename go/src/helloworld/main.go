9decd7d7216a09b190012618fdc9fc22682616b9
bluesDD <dd11blues@gmail.com>
2021-04-06 03:01:27 +0900

    :+1: multiple return

--- go/src/helloworld/main.go
+++ go/src/helloworld/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	sum := sum(os.Args[1], os.Args[2])
+	sum, _ := calc(os.Args[1], os.Args[2])
 	println("SUm: ", sum)
 }
 
@@ -14,5 +14,13 @@ func sum(number1 string, number2 string) (result int) {
 	int1, _ := strconv.Atoi(number1)
 	int2, _ := strconv.Atoi(number2)
 	result = int1 + int2
+	return
+}
+
+func calc(number1 string, number2 string) (sum int, mul int) {
+	int1, _ := strconv.Atoi(number1)
+	int2, _ := strconv.Atoi(number2)
+	sum = int1 + int2
+	mul = int1 + int2
 	return
 }
