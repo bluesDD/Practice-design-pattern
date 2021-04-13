155aa178abe95061ec75ae1e25a3b654ead512c7
bluesDD <dd11blues@gmail.com>
2021-04-14 04:01:34 +0900

    :+1: if

--- go/src/helloworld/main.go
+++ go/src/helloworld/main.go
@@ -1,30 +1,12 @@
 package main
 
 import (
-	"strconv"
+	"fmt"
 )
 
 func main() {
-	firstName := "John"
-	updateName(&firstName)
-	println(firstName)
-}
-
-func updateName(name *string) {
-	*name = "David"
-}
-
-func sum(number1 string, number2 string) (result int) {
-	int1, _ := strconv.Atoi(number1)
-	int2, _ := strconv.Atoi(number2)
-	result = int1 + int2
-	return
-}
-
-func calc(number1 string, number2 string) (sum int, mul int) {
-	int1, _ := strconv.Atoi(number1)
-	int2, _ := strconv.Atoi(number2)
-	sum = int1 + int2
-	mul = int1 + int2
-	return
+	x := 28
+	if x%2 == 0 {
+		fmt.Println(x, "is even")
+	}
 }
