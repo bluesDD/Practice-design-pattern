f181ee64788db0241e3d21a8299687a7619c04ac
bluesDD <dd11blues@gmail.com>
2021-04-26 21:47:32 +0900

    :+1: method

---
+++ go/src/playground/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var hex Hex = 100
+	fmt.Println(hex.String())
+}
+
+type Hex int
+
+func (h Hex) String() string {
+	return fmt.Sprintf("%x", int(h))
+}
